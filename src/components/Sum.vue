3dd99d3a28ae4434cefc7340241b847151b8affb
George Nguyen <nguyenquoctruongson@gmail.com>
2021-06-26 03:12:01 +0700

    Add Sum Component

---
+++ src/components/Sum.vue
@@ -0,0 +1,73 @@
+<template>
+	<div class="container mx-auto w-64">
+		<div class="flex justify-between">
+			<span class="w-20 text-left">A</span>
+			<input v-model="a" class="w-40 shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+		</div>
+		<div class="flex justify-between">
+			<span class="w-20 text-left">B</span>
+			<input v-model="b" class="w-40 shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+		</div>
+		<div class="flex justify-between">
+			<span class="w-20 text-left">SUM</span>
+			<span v-text="total" class="w-40 w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline text-left"></span>
+		</div>
+
+		<button v-on:click="handleSumClick" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+			Click
+		</button>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { store } from "../store";
+
+export default defineComponent({
+	name: "Sum",
+	methods: {
+		handleSumClick() {
+			store.dispatch("doSum");
+		},
+	},
+	computed: {
+		a: {
+			get() {
+				return store.state.a;
+			},
+			set(value) {
+				store.commit("updateA", value);
+			},
+		},
+		b: {
+			get() {
+				return store.state.b;
+			},
+			set(value) {
+				store.commit("updateB", value);
+			},
+		},
+		total() {
+			return store.state.total;
+		},
+	},
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+	margin: 40px 0 0;
+}
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #42b983;
+}
+</style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,34 @@
-import { createStore } from 'vuex'
+import { InjectionKey } from 'vue';
+import { createStore } from 'vuex';
 
-export default createStore({
+export interface State {
+  a: string,
+  b: string,
+  total: string
+}
+
+export const store = createStore<State>({
   state: {
+    a: "",
+    b: "",
+    total: ""
   },
   mutations: {
+    updateA(state, payload) {
+      state.a = payload;
+    },
+    updateB(state, payload) {
+      state.b = payload;
+    },
+    updateTotal(state, payload) {
+      state.total = payload
+    }
   },
   actions: {
-  },
-  modules: {
+    async doSum({ state, commit }) {
+      const total = +state.a + +state.b;
+      commit("updateTotal", total);
+    }
   }
 })
+
